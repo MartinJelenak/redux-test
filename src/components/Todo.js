08bb8024b82f713f2d46a330c2082a3b4226d1c9
Martin Jelenak <studiolang@gmail.com>
2021-02-24 23:22:39 +0100

    add popup menu, not completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,23 +4,51 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles((theme) => ({
 
 }));
 
+
+
 export default function Todo(props) {
     const classes = useStyles();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <ListItem key={props.id} button>
-            <ListItemText id={props.text} primary={props.text} />
-            <ListItemSecondaryAction>
-                <Checkbox
-                    checked={props.completed}
-                    edge="end"
-                    onChange={props.onClick}
-                />
-            </ListItemSecondaryAction>
-        </ListItem>
+        <>
+            <ListItem key={props.id} button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                <ListItemText id={props.text} primary={props.text} />
+                <ListItemSecondaryAction>
+                    <Checkbox
+                        checked={props.completed}
+                        edge="end"
+                        onChange={props.onClick}
+                    />
+                </ListItemSecondaryAction>
+            </ListItem>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>Edit</MenuItem>
+                <MenuItem onClick={handleClose}>Delete</MenuItem>
+            </Menu>
+        </>
     );
 }
